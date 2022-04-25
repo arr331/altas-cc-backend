8d3cea44935ee1751dbad2ddcd7bc68155b5d0dd
Adrian Ramirez Ramirez - Ceiba Software <adrian.ramirez@ceiba.com.co>
2022-04-25 07:56:38 -0500

    Se quita todo lo de lombok en todos los dto

--- microservicio/dominio/src/main/java/com/ceiba/moto/modelo/dto/DtoMoto.java
+++ microservicio/dominio/src/main/java/com/ceiba/moto/modelo/dto/DtoMoto.java
@@ -1,10 +1,5 @@
 package com.ceiba.moto.modelo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class DtoMoto {
     private Long id;
     private double precio;
@@ -15,4 +10,52 @@ public class DtoMoto {
     private String nombreImagen;
     private String nombreMoto;
     private int cantidad;
+
+    public DtoMoto(Long id, double precio, int cc, String marca, String estado, double descuento, String nombreImagen, String nombreMoto, int cantidad) {
+        this.id = id;
+        this.precio = precio;
+        this.cc = cc;
+        this.marca = marca;
+        this.estado = estado;
+        this.descuento = descuento;
+        this.nombreImagen = nombreImagen;
+        this.nombreMoto = nombreMoto;
+        this.cantidad = cantidad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public String getNombreImagen() {
+        return nombreImagen;
+    }
+
+    public String getNombreMoto() {
+        return nombreMoto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
 }
