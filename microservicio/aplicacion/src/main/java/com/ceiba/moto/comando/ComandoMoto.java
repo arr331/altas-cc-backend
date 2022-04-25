97006cbc4f7f805c11d57573f1a95c1fea1bbc6e
Adrian Ramirez Ramirez - Ceiba Software <adrian.ramirez@ceiba.com.co>
2022-04-25 07:51:08 -0500

    Se quita todo lo de lombok en comando moto

--- microservicio/aplicacion/src/main/java/com/ceiba/moto/comando/ComandoMoto.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/moto/comando/ComandoMoto.java
@@ -1,14 +1,5 @@
 package com.ceiba.moto.comando;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class ComandoMoto {
     private Long id;
     private double precio;
@@ -19,4 +10,92 @@ public class ComandoMoto {
     private String nombreImagen;
     private String nombreMoto;
     private int cantidad;
+
+    public ComandoMoto() {
+        // Es intencional
+    }
+
+    public ComandoMoto(Long id, double precio, int cc, String marca, String estado, double descuento, String nombreImagen, String nombreMoto, int cantidad) {
+        this.id = id;
+        this.precio = precio;
+        this.cc = cc;
+        this.marca = marca;
+        this.estado = estado;
+        this.descuento = descuento;
+        this.nombreImagen = nombreImagen;
+        this.nombreMoto = nombreMoto;
+        this.cantidad = cantidad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public String getNombreImagen() {
+        return nombreImagen;
+    }
+
+    public void setNombreImagen(String nombreImagen) {
+        this.nombreImagen = nombreImagen;
+    }
+
+    public String getNombreMoto() {
+        return nombreMoto;
+    }
+
+    public void setNombreMoto(String nombreMoto) {
+        this.nombreMoto = nombreMoto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
