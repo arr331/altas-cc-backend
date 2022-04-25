3ca2585300d7874d97510657527b41087713569d
Adrian Ramirez Ramirez - Ceiba Software <adrian.ramirez@ceiba.com.co>
2022-04-25 07:44:19 -0500

    Se quita todo lo de lombok en clase

--- microservicio/aplicacion/src/main/java/com/ceiba/compra/comando/ComandoCompra.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/compra/comando/ComandoCompra.java
@@ -1,12 +1,7 @@
 package com.ceiba.compra.comando;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
 public class ComandoCompra {
     private Long id;
     private Long idMoto;
@@ -31,6 +26,78 @@ public class ComandoCompra {
         this.valorTotal = valorTotal;
         this.abono = abono;
         this.codigo = codigo;
+        this.estado = estado;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdMoto() {
+        return idMoto;
+    }
+
+    public void setIdMoto(Long idMoto) {
+        this.idMoto = idMoto;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public double getAbono() {
+        return abono;
+    }
+
+    public void setAbono(double abono) {
+        this.abono = abono;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
         this.estado = estado;
     }
 }
