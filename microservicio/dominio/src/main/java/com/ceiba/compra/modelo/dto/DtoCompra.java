b102d0c8e36553c5bcdb0c4cfd758b5fac7fd185
Adrian Ramirez Ramirez - Ceiba Software <adrian.ramirez@ceiba.com.co>
2022-04-25 08:00:17 -0500

    Se pone todo lo de lombok en comando moto

--- microservicio/dominio/src/main/java/com/ceiba/compra/modelo/dto/DtoCompra.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/modelo/dto/DtoCompra.java
@@ -1,7 +1,12 @@
 package com.ceiba.compra.modelo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@AllArgsConstructor
 public class DtoCompra {
     private Long id;
     private Long idMoto;
@@ -12,52 +17,4 @@ public class DtoCompra {
     private double abono;
     private String codigo;
     private String estado;
-
-    public DtoCompra(Long id, Long idMoto, String cedula, String nombreCompleto, LocalDateTime fecha, double valorTotal, double abono, String codigo, String estado) {
-        this.id = id;
-        this.idMoto = idMoto;
-        this.cedula = cedula;
-        this.nombreCompleto = nombreCompleto;
-        this.fecha = fecha;
-        this.valorTotal = valorTotal;
-        this.abono = abono;
-        this.codigo = codigo;
-        this.estado = estado;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getIdMoto() {
-        return idMoto;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public String getNombreCompleto() {
-        return nombreCompleto;
-    }
-
-    public LocalDateTime getFecha() {
-        return fecha;
-    }
-
-    public double getValorTotal() {
-        return valorTotal;
-    }
-
-    public double getAbono() {
-        return abono;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
 }
