3430cb1ee65a120115727470ed10291066012525
Adrian Ramirez Ramirez - Ceiba Software <adrian.ramirez@ceiba.com.co>
2022-04-25 10:07:10 -0500

    Prueba

--- microservicio/dominio/src/main/java/com/ceiba/moto/modelo/dto/DtoMoto.java
+++ microservicio/dominio/src/main/java/com/ceiba/moto/modelo/dto/DtoMoto.java
@@ -1,10 +1,8 @@
 package com.ceiba.moto.modelo.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public class DtoMoto {
     private Long id;
     private double precio;
@@ -15,4 +13,40 @@ public class DtoMoto {
     private String nombreImagen;
     private String nombreMoto;
     private int cantidad;
+
+    public Long getId() {
+        return id;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public String getNombreImagen() {
+        return nombreImagen;
+    }
+
+    public String getNombreMoto() {
+        return nombreMoto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
 }
--- microservicio/dominio/src/main/java/com/ceiba/moto/modelo/entidad/Moto.java
+++ microservicio/dominio/src/main/java/com/ceiba/moto/modelo/entidad/Moto.java
@@ -1,10 +1,12 @@
 package com.ceiba.moto.modelo.entidad;
 
+import lombok.Generated;
 import lombok.Getter;
 
 import static com.ceiba.dominio.ValidadorArgumento.*;
 
 @Getter
+@Generated
 public final class Moto {
     private static final String SE_DEBE_INGRESAR_EL_PRECIO_DE_LA_MOTO = "El campo precio es obligatorio";
     private static final String SE_DEBE_INGRESAR_EL_CC_DE_LA_MOTO = "El campo cc es obligatorio";
