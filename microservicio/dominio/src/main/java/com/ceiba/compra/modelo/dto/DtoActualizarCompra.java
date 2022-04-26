a9e883750dffef4b0ac5a835233b252dac73ba13
Adrian Ramirez Ramirez - Ceiba Software <adrian.ramirez@ceiba.com.co>
2022-04-26 11:22:22 -0500

    Se generan get a dtoActualizaCompra

--- microservicio/dominio/src/main/java/com/ceiba/compra/modelo/dto/DtoActualizarCompra.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/modelo/dto/DtoActualizarCompra.java
@@ -7,4 +7,16 @@ public class DtoActualizarCompra {
     private Long id;
     private double abono;
     private String estado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public double getAbono() {
+        return abono;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
 }
